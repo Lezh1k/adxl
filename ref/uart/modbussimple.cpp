34791017df271f9cc07d6cf31b11d8819dcc36e0
A.Kiselev <lord_kern@mail.ru>
2017-09-20 22:15:09 +0600

    улучшения транспорта

--- ref/uart/modbussimple.cpp
+++ ref/uart/modbussimple.cpp
@@ -2,6 +2,14 @@
 #include "uart.h"
 #include <string.h>
 
+#define DEBUG 0
+#if DEBUG
+#define DBG(a) a
+#else
+#define DBG(a)
+#endif
+
+
 unsigned int crc16(const char *buf, int len)
 {
 
@@ -24,13 +32,15 @@ unsigned int crc16(const char *buf, int len)
 }
 
 
-
-ModBusSimple::ModBusSimple(int mySerial):
+ModBusSimple::ModBusSimple(int mySerial, int weakBroadcast):
     m_serial(mySerial),
+    m_weakBroadcast(weakBroadcast),
+    m_answerId(m_serial),
     m_pos(0)
 {
 
 }
+
 
 const char *ModBusSimple::extractPack()
 {
@@ -38,11 +48,13 @@ const char *ModBusSimple::extractPack()
     return m_pack;
 }
 
-void ModBusSimple::sendPack(unsigned char funcNum, unsigned int address, unsigned int value)
+void ModBusSimple::sendAknPack(unsigned char funcNum, unsigned int address, unsigned int value)
 {
+    if(m_answerId == 0 || m_answerId == m_weakBroadcast)
+        return;//на бродкасты не отвечаем
     char buf[10];
 
-    buf[0] = m_serial;
+    buf[0] = m_answerId;
     buf[1] = funcNum;
     buf[2] = address>>8;
     buf[3] = address&0xff;
@@ -55,7 +67,7 @@ void ModBusSimple::sendError(unsigned char funcNum, unsigned int errCode)
 {
     char buf[10];
 
-    buf[0] = m_serial;
+    buf[0] = m_answerId;
     buf[1] = funcNum | 0x80;
     buf[2] = errCode;
     sendDataWithCrc(buf,3);
@@ -72,14 +84,58 @@ void ModBusSimple::sendDataWithCrc(char *data, int len)
 }
 
 
-char ModBusSimple::getSerial() const
+char ModBusSimple::getId() const
 {
     return m_serial;
 }
 
-void ModBusSimple::setSerial(char serial)
+void ModBusSimple::setId(char id)
 {
-    m_serial = serial;
+    m_serial = id;
+}
+
+void ModBusSimple::partStart(unsigned char funcNum)
+{
+    m_pack[0] = m_answerId;
+    m_pack[1] = funcNum;
+    m_sendRegCount = 0;
+}
+
+void ModBusSimple::partNextReg(unsigned int value)
+{
+    unsigned int p = (m_sendRegCount++)*2 + 3;
+    if(p>sizeof(m_pack)-3)
+        return;
+    m_pack[p] = value>>8;
+    m_pack[p+1] = value&0xff;
+}
+
+void ModBusSimple::partSend()
+{
+    int len = m_sendRegCount*2;
+    m_pack[2] = len;
+    len += 3;
+    sendDataWithCrc(m_pack,len);
+}
+
+char ModBusSimple::getWeakBroadcast() const
+{
+    return m_weakBroadcast;
+}
+
+void ModBusSimple::setWeakBroadcast(char weakBroadcast)
+{
+    m_weakBroadcast = weakBroadcast;
+}
+
+char ModBusSimple::getAnswerId() const
+{
+    return m_answerId;
+}
+
+void ModBusSimple::setAnswerId(char answerId)
+{
+    m_answerId = answerId;
 }
 
 //void ModBusSimple::test()
@@ -98,17 +154,47 @@ void ModBusSimple::setSerial(char serial)
 //    //sendPack(0x0f,0x13,0x0a);
 //}
 
+#if DEBUG
+bool badbad(const char *reason){u->printf("[-%s-]",reason);return false;}
+#else
+#define badbad(a) false
+#endif
+
 bool ModBusSimple::testPack()
 {
     if(m_pos < 4 || m_pos>= sizeof(m_pack))
-        return false;
-    if(m_pack[0] != m_serial && m_pack[0] != 0)
-        return false;
+        return badbad("size");
+
+    DBG(u->puts("[PACK=");
+    for(unsigned int i=0;i<m_pos;i++){
+        u->hexByte(m_pack[i]);
+        u->puts(" ");
+    }
+    u->puts("] "););
+
+    if(m_pack[0] != m_serial && m_pack[0] != 0 && (m_weakBroadcast && m_pack[0] != m_weakBroadcast)){
+        DBG(u->printfn("[adr=%d,s=%d,w=%d]",m_pack[0],m_serial,m_weakBroadcast));
+        return badbad("address");
+    }
     unsigned int crc = crc16(m_pack,m_pos-2);
     unsigned int p = static_cast<unsigned char>(m_pack[m_pos-2]);
     p |= (static_cast<unsigned char>(m_pack[m_pos-1])<<8);
     if(p!=crc)
-        return false;
+        return badbad("crc");
+
+    unsigned char func = m_pack[1];
+    if(func & 0x80)
+        return badbad("errfunc");       // это ошибка другого устройства
+    if(func == 3 || func == 4){
+        if(m_pos & 1)
+            return badbad("other34");;   //это ответ других
+    }
+    if(func == 0x0F || func == 0x10){
+        if(!(m_pos & 1))
+            return badbad("other1516");;   //это ответ других
+    }
+
+
     return true;
 }
 
@@ -116,19 +202,60 @@ bool ModBusSimple::testPack()
 
 int ModBusSimple::getPackLen()
 {
+    if(m_isAlreadyPack){
+        m_isAlreadyPack = false;
+        if(testPack())
+            return m_pos;
+        return 0;
+    }
     while(u->kbhit()){
         int c = u->getch();
         if(c == -2){
+            DBG(u->puts("<##>"));
             if(m_pos != sizeof(m_pack)){
                 if(testPack()){
+                    DBG(u->printf("{%d}",m_pos));
                     return m_pos;
+                }else{
+                    DBG(u->puts("{--}"));
                 }
             }
             m_pos = 0;
+        }else{
+            DBG(u->putch('<'));
+            DBG(u->hexByte(c));
+            DBG(u->putch('>'));
+            if(m_pos < sizeof(m_pack)){
+                m_pack[m_pos++] = c;
+            }
         }
+    }
+    return 0;
+}
+
+int ModBusSimple::insertNewPack(const unsigned char *buf, int len)
+{
+    m_isAlreadyPack = true;
+    while(len--){
+        int c = *buf++;
         if(m_pos < sizeof(m_pack)){
             m_pack[m_pos++] = c;
         }
     }
+
+    if(m_pos != sizeof(m_pack)){
+        if(testPack()){
+            return m_pos;
+        }else{
+            u->puts("!test:");
+            for(unsigned int i=0;i<m_pos;i++){
+                u->hexByte(m_pack[i]);
+                u->putch(' ');
+            }
+            u->puts("\r\n");
+        }
+    }
+    m_pos = 0;
+
     return 0;
 }
--- ref/uart/modbussimple.h
+++ ref/uart/modbussimple.h
@@ -5,27 +5,71 @@
 class ModBusSimple
 {
 public:
-    ModBusSimple(int mySerial);
+    enum Errors{
+        ErrNoError = 0,
+        ErrUnknownFuncCode,
+        ErrAddressUnavailable,
+        ErrBadValue,
+        ErrFatalAction,
+        ErrInProcess,
+        ErrBusy,
+        ErrCantFunction,
+        ErrHardFault,
+    };
+
+    ModBusSimple(int mySerial, int weakBroadcast);
+
+    int insertNewPack(const unsigned char *buf, int len);
 
     int getPackLen();//принимаем, проверяем, если нет - то 0
     const char *extractPack();//зануляем буфер
-    void sendPack(unsigned char funcNum, unsigned int address, unsigned int value);
+    void sendAknPack(unsigned char funcNum, unsigned int address, unsigned int value);
     void sendError(unsigned char funcNum, unsigned int errCode);
 
     void test();
 
-    char getSerial() const;
+    char getId() const;
 
-    void setSerial(char serial);
+    void setId(char id);
+
+
+    void partStart(unsigned char funcNum);
+    void partNextReg(unsigned int value);
+    void partSend();
+
+    char getWeakBroadcast() const;
+    void setWeakBroadcast(char weakBroadcast);
+
+    char getAnswerId() const;
+    void setAnswerId(char answerId);
 
 private:
+    int m_sendRegCount;
     char m_serial;
+    char m_weakBroadcast;
     char m_pack[256];
+    char m_answerId;
     unsigned int m_pos;
+    bool m_isAlreadyPack = false;
 
     bool testPack();
 
     void sendDataWithCrc(char *data, int len);
 };
+
+
+//usage ChangeId(bus,32)->sendPack(1,2,3);
+class ChangeId{
+public:
+    ChangeId(ModBusSimple *bus, int serial):m_bus(bus),m_wasId(bus->getId()){bus->setId(serial);}
+    ~ChangeId(){m_bus->setId(m_wasId);}
+    inline ModBusSimple *operator->(){return m_bus;}
+private:
+    ModBusSimple *m_bus;
+    int m_wasId;
+};
+
+
+
 
 #endif // MODBUSSIMPLE_H
